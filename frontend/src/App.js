7aaa84e4354a757a9528baace8206d995b9ce8e6
alex-dembele <alexandredembele16@gmail.com>
2025-08-31 14:30:49 +0200

    Setup basic React frontend with Material-UI

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <h1>Deployment Control Center</h1>
+      <Button variant="contained">Test</Button>
+    </div>
+  );
+}
+
+export default App;
